6474de7d7bef17f961c95705bbe9ca662d5a11e9
JesusZelayaG <49107545+JesusZelayaG@users.noreply.github.com>
2021-07-26 07:38:46 -0600

    FIX: enviroment variable url

--- proyecto-front/src/environments/environment.prod.ts
+++ proyecto-front/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  url:'https://webapiproyecto20210723073816.azurewebsites.net/api'
 };
--- proyecto-front/src/environments/environment.ts
+++ proyecto-front/src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   url:'https://webapiproyecto20210723073816.azurewebsites.net/api'
 };
 
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
